0ce386297e25db51237f5122fc745cd5f17a8316
[dyad] <git@dyad.sh>
2025-09-30 20:25:51 +0530

    [dyad] Ensured main.tsx correctly renders App component and instructed user to Rebuild. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./globals.css";
+"use client";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Assuming you have an index.css for global styles like Tailwind
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
